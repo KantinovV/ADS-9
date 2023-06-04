4898ea16709ae579f7c68fea44699d7a948393f8
KantinovV <125280143+KantinovV@users.noreply.github.com>
2023-06-04 22:01:41 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -6,60 +6,61 @@
 #include <string>
 #include <cctype>
 #include <algorithm>
-template<typename T>
-class BST {
- private:
-  Node* vetka;
-  Node* insertNode(Node* vetka, const std::string& word) {
-    if (word < vetka->word) {
-      vetka->levo = insertNode(vetka->levo, word);
-    } else if (word > vetka->word) {
-      vetka->pravo = insertNode(vetka->pravo, word);
-    } else {
-      vetka->count++;
-    }
-    if (vetka == nullptr) {
-      return new Node(word);
-    }
-    return vetka;
-  }
-  Node* searchNode(Node* vetka, const std::string& word) {
-    if (word < vetka->word) {
-      return searchNode(vetka->levo, word);
-    } else {
-      return searchNode(vetka->pravo, word);
-    }
-        if (vetka == nullptr || root->word == word) {
-      return vetka;
-    }
-  }
-  int getDepth(Node* vetka) {
-    if (vetka == nullptr) {
-      return 0;
-    }
-    int levoDepth = getDepth(vetka->levo);
-    int pravoDepth = getDepth(vetka->pravo);
-    return std::max(levoDepth, pravoDepth) + 1;
-  }
 
- public:
-  BST() : vetka(nullptr) {}
-  void addElement(const std::string& word) {
-    vetka = insertNode(vetka, word);
-  }
-  int depth() {
-    return getDepth(root) - 1;
-  }
-  int search(const std::string& word) {
-    Node* node = searchNode(vetka, word);
-    return (node != nullptr) ? node->count : 0;
-  }
-};
 struct Node {
   std::string word;
   int count;
   Node* levo;
   Node* pravo;
   explicit Node(const std::string& word) : word(word), count(1), levo(nullptr), pravo(nullptr) {}
+};
+template<typename T>
+class BST {
+ private:
+  Node* root;
+  Node* insertNode(Node* root, const std::string& word) {
+    if (word < root->word) {
+      root->levo = insertNode(root->levo, word);
+    } else if (word > root->word) {
+      root->pravo = insertNode(root->pravo, word);
+    } else {
+      root->count++;
+    }
+    if (root == nullptr) {
+      return new Node(word);
+    }
+   
+    return root;
+  }
+  Node* searchNode(Node* root, const std::string& word) {
+    if (root == nullptr || root->word == word) {
+      return root;
+    }
+    if (word < root->word) {
+      return searchNode(root->levo, word);
+    } else {
+      return searchNode(root->pravo, word);
+    }
+  }
+  int datitiDepth(Node* root) {
+    if (root == nullptr) {
+      return 0;
+    }
+    int levoDepth = datitiDepth(root->levo);
+    int pravoDepth = datitiDepth(root->pravo);
+    return std::max(levoDepth, pravoDepth) + 1;
+  }
+ public:
+  BST() : root(nullptr) {}
+   int depth() {
+    return datitiDepth(root) - 1;
+  }
+   int sortirovka(const std::string& word) {
+    Node* node = searchNode(root, word);
+    return (node != nullptr) ? node->count : 0;
+  }
+  void addElement(const std::string& word) {
+    root = insertNode(root, word);
+  }
 };
 #endif  // INCLUDE_BST_H_
