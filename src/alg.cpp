3f67ba1942cde24212057da9b9869ff8b8d11ed8
KantinovV <125280143+KantinovV@users.noreply.github.com>
2023-06-04 23:34:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,29 +1,27 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
 #include  <fstream>
-#include  <locale>
-#include  <cstdlib>
 #include  "bst.h"
-
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
-  BST<std::string> tree;
-  std::ifstream file(filename);
-  std::string dobavit;
-  std::string liniy;
-  std::string tekst;
-  while (std::getline(file, liniy)) {
-    tekst = tekst + liniy + " ";
-  }
-  for (auto a : tekst) {
-    if (isalpha(a)) {
-      a = tolower(a);
-      dobavit += a;
-    } else {
-      tree.addElement(dobavit);
-      dobavit = "";
+    BST<std::string> bst;
+    std::ifstream file(filename);
+    std::string vihefdlhf;;
+    while (!file.eof()) {
+        char hytttytt = file.get();
+        if (hytttytt >= 65 && hytttytt <= 90) {
+            hytttytt += 32;
+            vihefdlhf += hytttytt;
+            continue;
+        }
+        if (hytttytt >= 97 && hytttytt <= 122) {
+            vihefdlhf += hytttytt;
+        } else {
+            if (!vihefdlhf.empty()) {
+                bst.addVal(vihefdlhf);
+            }
+            vihefdlhf.clear();
+        }
     }
-  }
-  file.close();
-  return tree;
+    bst.addVal(vihefdlhf);
+    file.close();
+    return bst;
 }
